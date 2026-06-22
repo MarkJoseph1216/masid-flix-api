c13e9bef4736ab9bc64b0a4a6ea6f6fb8453c352
MarkJoseph1216 <markcaron16@gmail.com>
2026-06-22 23:16:32 +0800

    Test with hardcoded credentials

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -143,24 +143,29 @@ class NotificationController extends Controller
     private function getAccessToken(array &$debug)
     {
         try {
-            $debug['steps'][] = 'Using Google Client library...';
+            $debug['steps'][] = 'Using hardcoded credentials for testing...';
             
-            $filePath = storage_path('app/firebase/firebase-credentials.json');
+            $credentials = [
+                'type' => 'service_account',
+                'project_id' => 'ichatyou',
+                'private_key_id' => '34205e0424080293bc30f9d938e0827b19843d76',
+                'private_key' => "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCxfkcwn1YPXU6M\n683FPiGFxbrEadAml4CnLCj3rgUSIpmimLANLQ2hR1nW1HbRhetNzuhiG8kWK5l8\nFfuPlcIeVwKcBotOb1C4MySF1uaWZxRWOGGVYFffHny6TAnSnZnebaxu+FaxX+d4\nWNyLgHdxCGiizhkviig4OZYbjAlGqB8n0T9OLqeL54LhlBbu/3Cd0MvYHxDUrXHi\n9udjdBTrcGoP+j2eZZ6F9lvDwsnA3l+FWgRxMgOG0JE8j7qJhQrVruz+JpOcg8D1\n1uMjLZKPsefHBaEo1rPiRoATlxdEVcJNxfhSYqgxk9e0L79vXhZ2xx8fa8z+VC3D\ncJ9YEGHFAgMBAAECggEAFWTmuNYj3fM9wh+0LFe6W7EDO6STeeteDwh0IbKgmth/\n00j7Q4NQNsXubsYqUkQFolnTyeuWd+0mcX4G1f5TqSuMvXjOdtRVEvbbKqTGI4/m\nNCRUotg7j0HR//SlZHUptFVc6P1XGcc5E9kGMGx6OS4typ30DDZndat/S++7uH/c\nvdEY2ahm3bXgaUcJ2vxQvemBwKrfCUdaEUvDDm6edvhVVaDPCPhpDiYlxqH+Q8DP\nId144KTGcRPvK5S4NJzyEQbfL0iABpCfA1FHnoR9m3ExhKHFLXS8fVaJijPYP36I\nIT7xG/3tJAtCnNf3nuhiC+XACMrGqMcWYcBacVJz4QKBgQDnaU+ey5plBf11agKx\n/Wo70475VyuoHnleZ9WpyeJA5PpjxJtxoCAm7onLxHu9aGk7k7MUL1GzR2+rfA3e\n9mizQeKpL1idf+F9ceQCnYU+5STTmPwtT4HaASkgVy7XjjnlucKQaE1Rafs6qpAN\npi4u+hCng64A8Acurcp2ZB3ZtQKBgQDEWlMyyZt8YeLKGpfz02wnvSPwBVy945O2\n579w1EkgTwbweC9IYQBbr+KG3ZC+zbRByQbCK7BIihjdV4MD3LPOoDIUx7OORwBa\n8qAxPRQHH28OD4AFJ7X6GxcXSDca5u/yKyQ9MBC7nYjwg9QU7F1r6OTw9Jd2B4kv\n/fv3vK4x0QKBgDZDs7AA/ouCBBVsboVeb3LoATbnAg3CV6OTpb7S4INnLnAGwoy6\nh8+ZUCbARGP9/+9Ai1XIYtgvgDguNvJ5xcODR6t0tsr4GeBYvKcAWSaOhTw5O6lE\nY6bDbuluiEVzzI/aJ43FZ5wXxhnTtUP+HAZYDV+6uSrvHkAL8NYiU/2hAoGAOctE\nVdyVkYTWVhqBw1jlqsS3QTyy0Ymcvudzp+g0Jfhc2Ibnk+xJSLN6f6vToPW3Ku3a\nuWhWmONc8jmB7K8Xlaf9VbR6G1S2vA5SLGwH6xjLfV3+loXbwGQc5dNxtM9orOUZ\n45C0PCTgW7rRv97amJqSWIIF2s3ZCXE+quq0cyECgYBinoj5AoXdZgzpNoR4Kz6Z\ngDuqxaVqZxFbZ5WkEEbCefOyV/R2sYtbFWKz+5v9rz3RXpbYuHJAO82Lxar0b657\nT64ZL4YcIuffxQCvJ+OAIeb5s+nVvlhG4xnJ3G9Qx9YwDpg7PUVsk/yHOI2CnWAT\n/TRy5KS7fUO7myjIBglDMw==\n-----END PRIVATE KEY-----\n",
+                'client_email' => 'firebase-adminsdk-fbsvc@ichatyou.iam.gserviceaccount.com',
+                'client_id' => '110210875032238205549',
+                'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+                'token_uri' => 'https://oauth2.googleapis.com/token',
+                'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+                'client_x509_cert_url' => 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40ichatyou.iam.gserviceaccount.com',
+                'universe_domain' => 'googleapis.com'
+            ];
             
-            if (!file_exists($filePath)) {
-                $debug['steps'][] = 'ERROR: Credentials file not found: ' . $filePath;
-                return null;
-            }
-            
-            $debug['steps'][] = 'Credentials file found';
+            $debug['steps'][] = 'Credentials hardcoded with private key length: ' . strlen($credentials['private_key']);
             
             $client = new \Google\Client();
-            
-            $client->setAuthConfig($filePath);
-            
+            $client->setAuthConfig($credentials);
             $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
             
-            $debug['steps'][] = 'Google Client configured with credentials';
+            $debug['steps'][] = 'Google Client configured with hardcoded credentials';
             
             $accessToken = $client->fetchAccessTokenWithAssertion();
             
@@ -170,7 +175,7 @@ class NotificationController extends Controller
                 return null;
             }
             
-            $debug['steps'][] = 'Access token obtained successfully with Google Client';
+            $debug['steps'][] = 'Access token obtained successfully with hardcoded credentials';
             return $accessToken['access_token'];
             
         } catch (\Exception $e) {
